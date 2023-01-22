f3630d21cbae0a282ada24b32681970440e874ea
hojat <hojat72elect@gmail.com>
2023-01-21 20:12:33 -0500

    initial commit

---
+++ desktop/src/ca/on/hojat/snakegame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ca.on.hojat.snakegame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ca.on.hojat.snakegame.SnakeGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Snake Game");
+		new Lwjgl3Application(new SnakeGame(), config);
+	}
+}
---
+++ ios/src/ca/on/hojat/snakegame/IOSLauncher.java
@@ -0,0 +1,22 @@
+package ca.on.hojat.snakegame;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import ca.on.hojat.snakegame.SnakeGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new SnakeGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
