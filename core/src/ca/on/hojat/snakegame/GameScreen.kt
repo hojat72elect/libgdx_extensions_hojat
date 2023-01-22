fe9e856aaaac7505135550bdffe859a9f4c2e37c
hojat <hojat72elect@gmail.com>
2023-01-22 01:58:33 -0500

    initial implementation of Snake game

--- core/src/ca/on/hojat/snakegame/GameScreen.kt
+++ core/src/ca/on/hojat/snakegame/GameScreen.kt
@@ -2,26 +2,25 @@ package ca.on.hojat.snakegame
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class GameScreen : ScreenAdapter() {
 
     private lateinit var batch: SpriteBatch
-    private lateinit var image: Texture
 
 
     override fun resize(width: Int, height: Int) {
         batch = SpriteBatch()
-        image = Texture("badlogic.jpg")
     }
 
     override fun render(delta: Float) {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
         batch.begin()
-        batch.draw(image, 0f, 0f)
+        // our rendering code will go here
         batch.end()
     }
 }
