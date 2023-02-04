4ef5aa30e10a3299c42e40937bdde28b7d231063
hojat <hojat72elect@gmail.com>
2023-02-04 00:04:00 -0500

    Started Flappy Bird

--- core/src/ca/on/hojat/snakegame/flappybird/FlappyBirdScreen.kt
+++ core/src/ca/on/hojat/snakegame/flappybird/FlappyBirdScreen.kt
@@ -1,9 +1,14 @@
 package ca.on.hojat.snakegame.flappybird
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
 import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.utils.viewport.FitViewport
 import com.badlogic.gdx.utils.viewport.Viewport
 
 class FlappyBirdScreen : ScreenAdapter() {
@@ -14,6 +19,40 @@ class FlappyBirdScreen : ScreenAdapter() {
     private lateinit var camera: Camera
     private lateinit var batch: SpriteBatch
 
+
+    override fun resize(width: Int, height: Int) {
+//        super.resize(width, height)
+        show()
+        viewPort.update(width, height)
+
+    }
+
+    /**
+     * all the warmup code happens here.
+     */
+    override fun show() {
+//        super.show()
+        camera = OrthographicCamera()
+        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0f)
+        camera.update()
+        viewPort = FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera)
+        shapeRenderer = ShapeRenderer()
+        batch = SpriteBatch()
+    }
+
+    override fun render(delta: Float) {
+//        super.render(delta)
+        clearScreen()
+        batch.projectionMatrix = camera.projection
+        batch.transformMatrix = camera.view
+        batch.begin()
+        batch.end()
+    }
+
+    private fun clearScreen() {
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    }
 
     companion object {
         const val WORLD_WIDTH = 480f
