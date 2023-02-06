037b6c2f5c62b0920f4dd11f0afe66990c7f373f
hojat <hojat72elect@gmail.com>
2023-02-06 15:27:28 -0500

    Config.java -> Config.kt

---
+++ core/src/ca/on/hojat/snakegame/gloweffect/config/Config.kt
@@ -0,0 +1,12 @@
+package ca.on.hojat.snakegame.gloweffect.config
+
+object Config {
+    // Core
+    const val WIDTH = 800
+    const val HEIGHT = 480
+
+    // Desktop
+    const val WINDOW_TITLE = "Glow Effect Example"
+    const val WINDOW_RESIZABLE = false;
+    const val WINDOW_FULLSCREEN = false;
+}
