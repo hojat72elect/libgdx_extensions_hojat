4c88c1a134af247170f9c201b155d3eedfd89fed
hojat <hojat72elect@gmail.com>
2023-02-18 02:20:52 -0500

    ShopScreen.java -> Kotlin

--- core/src/ca/on/hojat/snakegame/dragrace/shop/ShopScreen.kt
+++ core/src/ca/on/hojat/snakegame/dragrace/shop/ShopScreen.kt
@@ -1,166 +1,139 @@
-package ca.on.hojat.snakegame.dragrace.shop;
+package ca.on.hojat.snakegame.dragrace.shop
 
-import ca.on.hojat.snakegame.dragrace.Assets;
-import ca.on.hojat.snakegame.dragrace.MainStreet;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import ca.on.hojat.snakegame.dragrace.Settings;
-import ca.on.hojat.snakegame.dragrace.screens.MainMenuScreen;
-import ca.on.hojat.snakegame.dragrace.screens.Screens;
+import ca.on.hojat.snakegame.dragrace.Assets
+import ca.on.hojat.snakegame.dragrace.MainStreet
+import ca.on.hojat.snakegame.dragrace.Settings
+import ca.on.hojat.snakegame.dragrace.screens.MainMenuScreen
+import ca.on.hojat.snakegame.dragrace.screens.Screens
+import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.*
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 
-public class ShopScreen extends Screens {
+class ShopScreen(game: MainStreet?) : Screens(game) {
+    var btPersonajes: Button? = null
+    var btPowerUps: Button? = null
+    var btMonedas: Button? = null
+    var btNoAds: Button? = null
+    var btAtras: Button? = null
+    var lbCoin: Label
+    var scroll: ScrollPane
+    var contenedor: Table
 
-    Button btPersonajes, btPowerUps, btMonedas, btNoAds, btAtras;
-
-    Label lbCoin;
-
-    ScrollPane scroll;
-    Table contenedor;
-
-    public ShopScreen(final MainStreet game) {
-        super(game);
-
-        Label lbShop = new Label("Shop", Assets.labelStyleGrande);
-        lbShop.setSize(135, 50);
-        lbShop.setPosition(3, 747);
-
-        Image imgCoin = new Image(Assets.coinFrente);
-
-        lbCoin = new Label("0", Assets.labelStyleGrande);
-        lbCoin.setFontScale(.8f);
-
-        Table tbScores = new Table();
-        tbScores.setWidth(SCREEN_WIDTH);
-        tbScores.setPosition(0, SCREEN_HEIGHT - lbCoin.getHeight() / 2);
-        tbScores.padLeft(5).padRight(5);
-
-        tbScores.add(lbCoin).right().expand().padRight(5);
-        tbScores.add(imgCoin).right();
-
-        Image separadorH = new Image(Assets.separadorHorizontal);
-        separadorH.setSize(SCREEN_WIDTH, 5);
-        separadorH.setColor(Color.LIGHT_GRAY);
-        separadorH.setPosition(0, 740);
-
-        Image separadorV = new Image(Assets.separadorVertical);
-        separadorV.setSize(5, 745);
-        separadorV.setColor(Color.LIGHT_GRAY);
-        separadorV.setPosition(90, 0);
-
-        initButtons();
-
-        contenedor = new Table();
-        scroll = new ScrollPane(contenedor, Assets.styleScrollPane);
-        scroll.setSize(SCREEN_WIDTH - 95, (SCREEN_HEIGHT - 62));
-        scroll.setPosition(95, 0);
-
-        stage.addActor(tbScores);
-        stage.addActor(lbShop);
-        stage.addActor(separadorV);
-        stage.addActor(separadorH);
-        stage.addActor(btPersonajes);
-
-        stage.addActor(btMonedas);
-        stage.addActor(btNoAds);
-        stage.addActor(btAtras);
-        stage.addActor(scroll);
-
-        new PersonajesSubMenu(game, contenedor);
-
+    init {
+        val lbShop = Label("Shop", Assets.labelStyleGrande)
+        lbShop.setSize(135f, 50f)
+        lbShop.setPosition(3f, 747f)
+        val imgCoin = Image(Assets.coinFrente)
+        lbCoin = Label("0", Assets.labelStyleGrande)
+        lbCoin.setFontScale(.8f)
+        val tbScores = Table()
+        tbScores.width = SCREEN_WIDTH.toFloat()
+        tbScores.setPosition(0f, SCREEN_HEIGHT - lbCoin.height / 2)
+        tbScores.padLeft(5f).padRight(5f)
+        tbScores.add(lbCoin).right().expand().padRight(5f)
+        tbScores.add(imgCoin).right()
+        val separadorH = Image(Assets.separadorHorizontal)
+        separadorH.setSize(SCREEN_WIDTH.toFloat(), 5f)
+        separadorH.color = Color.LIGHT_GRAY
+        separadorH.setPosition(0f, 740f)
+        val separadorV = Image(Assets.separadorVertical)
+        separadorV.setSize(5f, 745f)
+        separadorV.color = Color.LIGHT_GRAY
+        separadorV.setPosition(90f, 0f)
+        initButtons()
+        contenedor = Table()
+        scroll = ScrollPane(contenedor, Assets.styleScrollPane)
+        scroll.setSize((SCREEN_WIDTH - 95).toFloat(), (SCREEN_HEIGHT - 62).toFloat())
+        scroll.setPosition(95f, 0f)
+        stage.addActor(tbScores)
+        stage.addActor(lbShop)
+        stage.addActor(separadorV)
+        stage.addActor(separadorH)
+        stage.addActor(btPersonajes)
+        stage.addActor(btMonedas)
+        stage.addActor(btNoAds)
+        stage.addActor(btAtras)
+        stage.addActor(scroll)
+        PersonajesSubMenu(game, contenedor)
     }
 
-    private void initButtons() {
-        btPersonajes = new Button(
-                new TextureRegionDrawable(Assets.carroTornado));
-        btPersonajes.setSize(45, 65);
-        btPersonajes.setPosition(23, 660);
-        addEfectoPress(btPersonajes);
-        btPersonajes.addListener(new ClickListener() {
-            public void clicked(
-                    com.badlogic.gdx.scenes.scene2d.InputEvent event, float x,
-                    float y) {
-                new PersonajesSubMenu(game, contenedor);
+    private fun initButtons() {
+        btPersonajes = Button(
+            TextureRegionDrawable(Assets.carroTornado)
+        )
+        btPersonajes!!.setSize(45f, 65f)
+        btPersonajes!!.setPosition(23f, 660f)
+        addEfectoPress(btPersonajes)
+        btPersonajes!!.addListener(object : ClickListener() {
+            override fun clicked(
+                event: InputEvent, x: Float,
+                y: Float
+            ) {
+                PersonajesSubMenu(game, contenedor)
             }
-
-            ;
-        });
-
-        btPowerUps = new Button(new TextureRegionDrawable(Assets.carroTornado));
-        btPowerUps.setSize(55, 55);
-        btPowerUps.setPosition(17, 570);
-        addEfectoPress(btPowerUps);
-        btPowerUps.addListener(new ClickListener() {
-            public void clicked(
-                    com.badlogic.gdx.scenes.scene2d.InputEvent event, float x,
-                    float y) {
-
+        })
+        btPowerUps = Button(TextureRegionDrawable(Assets.carroTornado))
+        btPowerUps!!.setSize(55f, 55f)
+        btPowerUps!!.setPosition(17f, 570f)
+        addEfectoPress(btPowerUps)
+        btPowerUps!!.addListener(object : ClickListener() {
+            override fun clicked(
+                event: InputEvent, x: Float,
+                y: Float
+            ) {
             }
-        });
-
-        btMonedas = new Button(new TextureRegionDrawable(Assets.coinFrente));
-        btMonedas.setSize(55, 55);
-        btMonedas.setPosition(17, 480);
-        addEfectoPress(btMonedas);
-        btMonedas.addListener(new ClickListener() {
-            public void clicked(
-                    com.badlogic.gdx.scenes.scene2d.InputEvent event, float x,
-                    float y) {
-                new GetCoinsSubMenu(game, contenedor);
+        })
+        btMonedas = Button(TextureRegionDrawable(Assets.coinFrente))
+        btMonedas!!.setSize(55f, 55f)
+        btMonedas!!.setPosition(17f, 480f)
+        addEfectoPress(btMonedas)
+        btMonedas!!.addListener(object : ClickListener() {
+            override fun clicked(
+                event: InputEvent, x: Float,
+                y: Float
+            ) {
+                GetCoinsSubMenu(game, contenedor)
             }
-
-            ;
-        });
-
-        btNoAds = new Button(new TextureRegionDrawable(Assets.btNoAds));
-        btNoAds.setSize(55, 55);
-        btNoAds.setPosition(17, 390);
-        addEfectoPress(btNoAds);
-        btNoAds.addListener(new ClickListener() {
-            public void clicked(
-                    com.badlogic.gdx.scenes.scene2d.InputEvent event, float x,
-                    float y) {
-                new NoAdsSubMenu(game, contenedor);
+        })
+        btNoAds = Button(TextureRegionDrawable(Assets.btNoAds))
+        btNoAds!!.setSize(55f, 55f)
+        btNoAds!!.setPosition(17f, 390f)
+        addEfectoPress(btNoAds)
+        btNoAds!!.addListener(object : ClickListener() {
+            override fun clicked(
+                event: InputEvent, x: Float,
+                y: Float
+            ) {
+                NoAdsSubMenu(game, contenedor)
             }
-
-            ;
-        });
-
-        btAtras = new Button(new TextureRegionDrawable(Assets.btAtras));
-        btAtras.setSize(55, 55);
-        btAtras.setPosition(17, 10);
-        addEfectoPress(btAtras);
-        btAtras.addListener(new ClickListener() {
-            public void clicked(
-                    com.badlogic.gdx.scenes.scene2d.InputEvent event, float x,
-                    float y) {
-                changeScreenWithFadeOut(MainMenuScreen.class, game);
+        })
+        btAtras = Button(TextureRegionDrawable(Assets.btAtras))
+        btAtras!!.setSize(55f, 55f)
+        btAtras!!.setPosition(17f, 10f)
+        addEfectoPress(btAtras)
+        btAtras!!.addListener(object : ClickListener() {
+            override fun clicked(
+                event: InputEvent, x: Float,
+                y: Float
+            ) {
+                changeScreenWithFadeOut(MainMenuScreen::class.java, game)
             }
-
-            ;
-        });
-
+        })
     }
 
-    @Override
-    public void draw(float delta) {
-
+    override fun draw(delta: Float) {}
+    override fun update(delta: Float) {
+        lbCoin.setText(Settings.coinsTotal.toString() + "")
     }
 
-    @Override
-    public void update(float delta) {
-        lbCoin.setText(Settings.coinsTotal + "");
-    }
-
-    @Override
-    public boolean keyDown(int tecleada) {
-        if (tecleada == Keys.BACK || tecleada == Keys.ESCAPE) {
-            changeScreenWithFadeOut(MainMenuScreen.class, game);
-            return true;
+    override fun keyDown(tecleada: Int): Boolean {
+        if (tecleada == Input.Keys.BACK || tecleada == Input.Keys.ESCAPE) {
+            changeScreenWithFadeOut(MainMenuScreen::class.java, game)
+            return true
         }
-        return false;
+        return false
     }
-
 }
