6a84195aedf0ee2a9134617ee5baeef82061f3a2
hojat <hojat72elect@gmail.com>
2023-02-20 22:39:32 -0500

    cleaned

--- android/src/ca/on/hojat/snakegame/AndroidLauncher.java
+++ android/src/ca/on/hojat/snakegame/AndroidLauncher.java
@@ -2,7 +2,6 @@ package ca.on.hojat.snakegame;
 
 import android.os.Bundle;
 
-import ca.on.hojat.snakegame.snakegame.SnakeGame;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
@@ -11,6 +10,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SnakeGame(), config);
+//		initialize(new MainStreet(), config);
 	}
 }
